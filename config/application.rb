51af53a5ec49d29e156d8494380504acf63ec1fa
Emil Kampp <emil@kampp.me>
2014-06-05 01:28:41 +0200

    Setup database connection

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,11 @@
 require File.expand_path("../boot", __FILE__)
 
+# Setup connection to db
+require 'active_record'
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.configurations = YAML::load(IO.read(Lattice.root.join('config', 'database.yml')))
+ActiveRecord::Base.establish_connection(ENV['RACK_ENV'])
+
 # Require your resources here
 require 'resources/objects'
 
